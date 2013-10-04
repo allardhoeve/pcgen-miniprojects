ee751ba5d4acfe0a4d24ef8e4194a7d08b35af29
Allard Hoeve <allard@byte.nl>
2013-10-04 23:05:41 +0200

    Parse ALL the spells in one function

--- pcgen/__init__.py
+++ pcgen/__init__.py
@@ -1,1 +1,13 @@
-__author__ = 'allard'
+from pcgen.campaign import PathfinderCampaign
+from pcgen.parser import parse_spells
+
+
+def find_all_pathfinder_spells():
+    campaign = PathfinderCampaign()
+    spell_listfiles = campaign.find_spell_listfiles()
+    spells = []
+
+    for lst in spell_listfiles:
+        spells = spells + parse_spells(lst)
+
+    return spells
