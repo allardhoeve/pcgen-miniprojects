8eccdf132999ba391f2e786f8768166e88e997bb
Allard Hoeve <allard@byte.nl>
2013-10-13 08:50:09 +0200

    Move optimizations to pcgen.fuzzy.match

--- pcgen/fuzzy/__init__.py
+++ pcgen/fuzzy/__init__.py
@@ -1,8 +1,43 @@
 import re
 from fuzzywuzzy.fuzz import WRatio
-from fuzzywuzzy.process import extractOne as match
+from fuzzywuzzy.process import extractOne
 
-assert match  # used by QA classes
+
+def match(query, choices, processor=None, scorer=None, score_cutoff=0):
+    """
+    Case-optimize the fuzzy-matcher.
+
+    Use canonical Pathfinder names, to instantly find spells:
+     - Aid (Mass) -> Aid, Mass
+     - Restoration (Lesser) -> Restoration, Lesser
+     - Command (Greater) -> Command, Greater
+     - Endure Elements (Communal) -> Endure Elements, Communal
+
+    Also try to match restricted spells to their normal counterparts:
+     - Align Weapon (Chaos Only) -> Align Weapon
+
+    Finally, skip any Masterpieces (they are not on the PRD):
+     - Masterpiece (Depths of the Mountain) -> None
+
+    arguments:
+     - arguments are like fuzzywuzzy.process.extractOne
+     - notable arguments:
+       - query: the name of the spell to find
+       - choices: the group to match the name to
+
+    returns:
+     - tuple of (matchedname, certainty)
+    """
+
+    # Strip (Jadda Only) from query name
+    query = re.sub("\([^)]+ only\)", "", query, flags=re.I)
+
+    return extractOne(
+        query,
+        choices,
+        processor=processor,
+        scorer=scorer,
+        score_cutoff=score_cutoff)
 
 
 def SpellRatio(s1, s2, *args, **kwargs):
@@ -20,6 +55,13 @@ def SpellRatio(s1, s2, *args, **kwargs):
     cases.
 
     Then, let WRatio decide.
+
+    arguments:
+     - s1: name to find
+     - s2: candidate from extrator choices
+     - other arguments are passed to fuzzywuzzy.fuzz.WRatio
+
+    returns:
+     - integer with probability of match
     """
-    s1 = re.sub("\([^)]+ only\)", "", s1, flags=re.I)
     return WRatio(s1, s2, *args, **kwargs)
