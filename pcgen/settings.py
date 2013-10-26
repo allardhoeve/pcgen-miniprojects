4a94aa9aac3ac2bacea654507520938b77f61fe8
Allard Hoeve <allard@byte.nl>
2013-10-26 09:32:42 +0200

    Increase coverage

--- pcgen/settings.py
+++ pcgen/settings.py
@@ -6,10 +6,10 @@ DATADIR = os.environ.get("PCGEN_DATADIR", None)
 
 try:
     from .settings_local import *
-except ImportError:
+except ImportError:  # pragma: no cover
     pass
 
-if not DATADIR:
-    raise ImproperlyConfigured("No DATADIR defined and PCGEN_DATADIR not set in environment")
+if not DATADIR:  # pragma: no cover
+    raise RuntimeError("No DATADIR defined and PCGEN_DATADIR not set in environment")
 
 DATADIR = Path(DATADIR)
