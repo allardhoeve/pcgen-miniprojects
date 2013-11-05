6f2e90bf6f72105a29096c68e36f7047f4ecd166
Allard Hoeve <allard@byte.nl>
2013-11-05 23:15:06 +0100

    Parse all pathfinder feats

--- prd/__init__.py
+++ prd/__init__.py
@@ -4,13 +4,29 @@ import requests
 from prd.dict import CaseInsensitiveDict
 
 
+def get_prd_feat_links(html=None):
+    if not html:
+        html = fetch_prd_feat_index()
+
+    def clean_feat_name(name, link):
+        name = re.sub(r'\s+\([^)]+\)$', '', name)
+        return name, link
+
+    # yes, really, spell :(
+    return get_prd_object_links(html, "spell", "Feats", filter=clean_feat_name)
+
+
 def get_prd_spell_links(html=None):
 
     if not html:
         html = fetch_prd_spell_index()
 
+    return get_prd_object_links(html, "spell", "Spells")
+
+
+def get_prd_object_links(html, type, header, filter=None):
     root = etree.HTML(html)
-    links = root.xpath("//div[@id='spell-index-wrapper']/ul/li/a")
+    links = root.xpath("//div[@id='%s-index-wrapper']/ul/li/a" % type)
 
     ret = CaseInsensitiveDict()
 
@@ -18,13 +34,22 @@ def get_prd_spell_links(html=None):
         name = link.text.strip()
 
         # skip all index links
-        if re.match(r"^[A-Z] Spells$", name):
+        if re.match(r"^[A-Z] %s$" % header, name):
             continue
 
         url = "http://paizo.com" + link.attrib['href']
+
+        if filter:
+            name, url = filter(name, url)
+
         ret[name] = url
 
     return ret
+
+
+def fetch_prd_feat_index():
+    index = requests.get("http://paizo.com/pathfinderRPG/prd/indices/feats.html")
+    return index
 
 
 def fetch_prd_spell_index():
---
+++ tests/unit/prd/test_get_prd_feat_links.py
@@ -0,0 +1,37 @@
+from unittest import skip
+from pcgen import settings
+from pcgen.testcase import TestCase
+from prd import get_prd_feat_links
+
+
+class TestPRD(TestCase):
+
+    def setUp(self):
+        with open(settings.DATADIR.child("pathfinder").child("feats.html")) as fh:
+            self.indexcontent = fh.read()
+
+        self.mock_get = self.set_up_patch('requests.get')
+        self.mock_get.return_value = self.indexcontent
+
+    def test_get_prd_spell_links_fetches_prd_spell_index(self):
+        get_prd_feat_links()
+        self.mock_get.assert_called_once_with('http://paizo.com/pathfinderRPG/prd/indices/feats.html')
+
+    def test_get_prd_spell_links_return_contains_iron_will(self):
+        spells = get_prd_feat_links()
+        self.assertEqual(spells["Iron Will"], "http://paizo.com/pathfinderRPG/prd/feats.html#_iron-will")
+
+    def test_get_prd_spell_links_finds_adjuring_step(self):
+        spells = get_prd_feat_links()
+        self.assertIn("Twin Thunders", spells)
+        self.assertEqual(spells["Twin Thunders"], "http://paizo.com/pathfinderRPG/prd/ultimateCombat/ultimateCombatFeats.html#_twin-thunders")
+
+    def test_get_prd_spell_links_is_caseinsensitive(self):
+        spells = get_prd_feat_links()
+        self.assertIn("Iron Will", spells)
+        self.assertIn("iron will", spells)
+        self.assertIn("iRon WilL", spells)
+
+    def test_get_prd_spells_skips_letter_indices(self):
+        spells = get_prd_feat_links()
+        self.assertNotIn("A Feats", spells)
