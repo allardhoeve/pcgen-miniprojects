d3b67ebc1899b9dd8437e4dfface6b51d62a3c0b
Allard Hoeve <allardhoeve@gmail.com>
2015-04-29 21:44:14 +0200

    All arcane spells that are also domain spells

--- arcane_domain_spells.py
+++ arcane_domain_spells.py
@@ -2,17 +2,17 @@
 from pcgen import find_all_pathfinder_spells
 
 
-def domain_spell_but_not_cleric_spell(spell):
-    return spell.domains and spell.classes and not "Cleric" in spell.classes
+def domain_spell_but_not_cleric_spell(s):
+    return s.domains and s.classes and "Cleric" not in s.classes
 
 
-def spell_is_wizard_spell(spell):
-    return "Wizard" in spell.classes
+def spell_is_wizard_spell(s):
+    return "Wizard" in s.classes
 
 
 spells = find_all_pathfinder_spells()
 domain_spells = filter(spell_is_wizard_spell, filter(domain_spell_but_not_cleric_spell, spells))
 
 
-for spell in sorted(domain_spells, key=lambda spell: (spell.school, spell.name)):
+for spell in sorted(domain_spells, key=lambda s: (s.school, s.name)):
     print "%s [%s] is level %s: available at %s" % (spell.name, spell.school.lower(), spell.classes['Wizard'], spell.domains)
